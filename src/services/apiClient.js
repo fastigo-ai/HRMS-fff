a47e970d7046ded2e701f867e378a39422d2d83f
fastigo-ai <fastigo.in@gmail.com>
2026-06-03 16:38:35 +0530

    push1

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8000/api";
+export const API_BASE_URL = "https://hrms-bb.onrender.com/api";
 
 export const authenticatedFetch = async (url, options = {}) => {
   options.credentials = "include";
